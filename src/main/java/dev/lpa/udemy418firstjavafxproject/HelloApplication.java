21eb0b3ed0ef6723cc530a1d5ec076c2acbdcb72
Michael Kopp <MichaelJG.Kopp@gmail.com>
2024-11-13 22:42:49 +0100

    can change ui in Main Application, will focus on fxml though

--- src/main/java/dev/lpa/udemy418firstjavafxproject/HelloApplication.java
+++ src/main/java/dev/lpa/udemy418firstjavafxproject/HelloApplication.java
@@ -1,13 +1,8 @@
 package dev.lpa.udemy418firstjavafxproject;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,20 +10,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException { // stage is window or container
-//    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
     
-    GridPane root = new GridPane(); // fxmlLoader
-    root.setAlignment(Pos.CENTER);
-    root.setVgap(10);
-    root.setHgap(10);
+//    GridPane root = new GridPane(); // fxmlLoader
+//    root.setAlignment(Pos.CENTER);
+//    root.setVgap(10);
+//    root.setHgap(10);
+//
+//    Label greeting = new Label("Welcome to JavaFX!");
+//    greeting.setTextFill(Color.GREEN);
+//    greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//    root.getChildren().add(greeting); // add label to list of children
+//    Scene scene = new Scene(root, 700, 275); // like a theater, scene is what is seen on the window
     
-    Label greeting = new Label("Welcome to JavaFX!");
-    greeting.setTextFill(Color.GREEN);
-    greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-    
-    root.getChildren().add(greeting); // add label to list of children
-    
-    Scene scene = new Scene(root, 700, 275); // like a theater, scene is what is seen on the window
+    Scene scene = new Scene(fxmlLoader.load(), 700, 275); // like a theater, scene is what is seen on the window
     stage.setTitle("Hello JavaFX!");
     stage.setScene(scene);
     stage.show();
